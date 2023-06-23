5e8ae7032c756a4f55cafc36b8d77f7708ef1730
Jinwoo Kim <jinwoo.kim@student.curtin.edu.au>
2023-06-23 16:52:24 +0800

    Add files via upload

---
+++ Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace JokesWeb.Models
+{
+	public class Joke
+	{
+		public int Id { get; set; }
+
+		public string JokeQuestion { get; set; }
+
+		public string JokeAnswer { get; set; }
+
+
+		public Joke()
+		{
+
+		}
+
+	}
+}
+
